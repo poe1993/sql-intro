fb5736efb074e26849a2154b861ecb256645e1ad
poe1993 <poe1993@yahoo.com>
2020-08-27 08:04:25 -0400

    Homework

--- queries.sql
+++ queries.sql
@@ -39,4 +39,78 @@ INSERT INTO "Employees"("fullname","salary","jobposition","phoneextension","ispa
  5.DELETE FROM "Employees" WHERE "FullName" = 'Lazy Larry';
 
  6.ALTER TABLE "Employees" ADD COLUMN "ParkingSpot" VARCHAR(10);
+
+SQL-JOIN HOMEWORK:
  
+CREATE TABLE "Departments" (
+ "Id" SERIAL PRIMARY KEY,
+ "DepartmentName" TEXT,
+ "Building" TEXT
+ );
+
+ CREATE TABLE "Products" (
+ "Id" SERIAL PRIMARY KEY,
+ "Price" FLOAT,
+ "Name" TEXT,
+ "Description" TEXT,
+ "QuantityInStock" INT
+ );
+
+CREATE TABLE "Orders" (
+ "Id" SERIAL PRIMARY KEY,
+ "OrderNumber" TEXT,
+ "DatePlaced" TIMESTAMP,
+ "Email" TEXT
+ );
+
+ truncate table "Employees"
+
+ ALTER TABLE "Employees" ADD COLUMN "DepartmentId" INTEGER NULL REFERENCES "Departments" ("Id");
+
+ CREATE TABLE "ProductOrders" (
+ "OrderId" INTEGER NULL REFERENCES "Orders" ("Id"),
+ "ProductId" INTEGER NULL REFERENCES "Products" ("Id"),
+ "OrderQuantity" INT
+ );
+
+ INSERT INTO "Departments" ("DepartmentName","Building")
+ VALUES ('Development','Main');
+ INSERT INTO "Departments" ("DepartmentName","Building")
+ VALUES ('Marketing','North');
+
+ INSERT INTO "Employees" ("FullName","Salary","JobPosition","PhoneExtension","IsPartTime","DepartmentId")
+VALUES ('Tim Smith',40000,'Programmer','123',True,'1');
+
+INSERT INTO "Employees" ("FullName","Salary","JobPosition","PhoneExtension","IsPartTime","DepartmentId")
+VALUES ('Barbara Ramsey',80000,'Manager','234',False,'1');
+
+INSERT INTO "Employees" ("FullName","Salary","JobPosition","PhoneExtension","IsPartTime","DepartmentId")
+VALUES ('Tom Jones',32000,'Admin','456',True,'2');
+
+INSERT INTO "Products" ("Price","Name","Description","QuantityInStock")
+VALUES (12.45,'Widget','The Original Widget',100);
+
+INSERT INTO "Products" ("Price","Name","Description","QuantityInStock")
+VALUES (99.99,'Flowbee','Perfect for haircuts', 3);
+
+1.
+INSERT INTO "Orders" ("OrderNumber","DatePlaced","Email")
+VALUES ('X529','2020-01-01 16:55:00','person@exmaple.com');
+2.
+INSERT INTO "ProductOrders" ("OrderId","ProductId","OrderQuantity")
+VALUES (‘1’,‘1’,3);
+3.
+INSERT INTO "ProductOrders" ("OrderId","ProductId","OrderQuantity")
+VALUES (‘1’,‘2’, 2);
+4.
+SELECT "FullName" FROM "Employees" WHERE "DepartmentId" = '1';
+5.
+SELECT "PhoneExtension" FROM "Employees" WHERE "DepartmentId" = '1';
+6.
+SELECT "OrderId" FROM "ProductOrders" WHERE "ProductId" = '2';
+7.
+DELETE FROM "ProductOrders" WHERE "ProductId" = '2';
+
+
+
+
